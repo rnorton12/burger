92e84796285b0d729a9cf0013a2e8f6ec8273ac5
Roy Norton <rnorton12@centurylink.net>
2018-02-11 15:11:31 -0700

    setup directory structure and placeholder files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Double Decker');
